39ef93022d0aa396a0098db377e62019803932a3
Paida-zoila <paidachinama@gmail.com>
2021-07-25 14:51:40 -0700

    handling conversion specifiers

---
+++ _printf.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* _printf - prints according to a format
+* @format: character string
+* Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+int count_chars;
+conver_t f_list[] = {
+{"c", print_char},
+{"s", print_string},
+{"%", print_percent},
+{NULL, NULL}
+};
+va_list arg_list;
+if (format == NULL)
+return (-1);
+
+va_start(arg_list, format);
+count_chars = parser(format, f_list, arg_list);
+va_end(arg_list);
+
+return (count_chars);
+}
