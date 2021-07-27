b3766583cede7925f983f75c940d86f6e12f2973
Paida-zoila <paidachinama@gmail.com>
2021-07-27 13:35:23 -0700

    final commit to master after merging

--- string_handling.c
+++ string_handling.c
@@ -9,20 +9,20 @@
 int print_reversed(va_list arg)
 
 {
-  int len;
-  char *str;  
-  char *ptr;
+	int len;
+	char *str;
+	char *ptr;
 
-  str = va_arg(arg, char *);
-  if (str == NULL)
-    return (-1);
-  ptr = rev_string(str);
-  if (ptr == NULL)
-    return (-1);
-  for (len = 0; ptr[len] != '\0'; len++)
-    _putchar(ptr[len]);
-  free(ptr);
-  return (len);  
+	str = va_arg(arg, char *);
+	if (str == NULL)
+	return (-1);
+	ptr = rev_string(str);
+	if (ptr == NULL)
+	return (-1);
+	for (len = 0; ptr[len] != '\0'; len++)
+	_putchar(ptr[len]);
+	free(ptr);
+	return (len);
 }
 
 /**
@@ -34,27 +34,27 @@ int print_reversed(va_list arg)
 int rot13(va_list list)
 
 {
-  int i;
-  int x;
-  char *str;
-  char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
-  char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
- 
-  str = va_arg(list, char *);
-  if (str == NULL)
-    return (-1);
-  for (i = 0; str[i] != '\0'; i++)
-    {
-      for (x = 0; x <= 52; x++)
+	int i;
+	int x;
+	char *str;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(list, char *);
+	if (str == NULL)
+	return (-1);
+	for (i = 0; str[i] != '\0'; i++)
 	{
-	  if (str[i] == s[x])
-	    {
-	      _putchar(u[x]);
-	      break; 
-	    } 
+	for (x = 0; x <= 52; x++)
+	{
+	if (str[i] == s[x])
+		{
+		_putchar(u[x]);
+		break;
+		}
 	}
-      if (x == 53)
-	_putchar(str[i]); 
-    }
-  return (i)
+	if (x == 53)
+	_putchar(str[i]);
+	}
+	return (i)
 }
