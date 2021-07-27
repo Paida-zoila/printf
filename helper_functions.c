3df6ed9ec546fd12dca937cb0c6fe0448ab39524
Paida-zoila <paidachinama@gmail.com>
2021-07-27 12:53:48 -0700

    betty style

--- helper_functions.c
+++ helper_functions.c
@@ -11,24 +11,24 @@
 char *rev_string(char *s)
 
 {
-  int len;
- int head; 
-  char tmp;  
-  char *dest;
+int len;
+int head;
+char tmp;
+char *dest;
 
-  for (len = 0; s[len] != '\0'; len++)
-    {}
-  dest = malloc(sizeof(char) * len + 1);
-  if (dest == NULL)
-    return (NULL);
-  _memcpy(dest, s, len);
-  for (head = 0; head < len; head++, len--)
-    {
-      tmp = dest[len - 1];
-      dest[len - 1] = dest[head]; 
-      dest[head] = tmp;
-    }
-  return (dest);
+for (len = 0; s[len] != '\0'; len++)
+{}
+dest = malloc(sizeof(char) * len + 1);
+if (dest == NULL)
+return (NULL);
+_memcpy(dest, s, len);
+for (head = 0; head < len; head++, len--)
+{
+tmp = dest[len - 1];
+dest[len - 1] = dest[head];
+dest[head] = tmp;
+}
+return (dest);
 }
 
 /**
@@ -39,9 +39,9 @@ char *rev_string(char *s)
 void write_base(char *str)
 
 {
-  int i;
-  for (i = 0; str[i] != '\0'; i++)
-    _putchar(str[i]);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
 }
 
 /**
@@ -54,12 +54,12 @@ void write_base(char *str)
 unsigned int base_len(unsigned int num, int base)
 
 {
-  unsigned int i;
-  for (i = 0; num > 0; i++)
-    {
-      num = num / base; 
-    }
-  return (i);
+unsigned int i;
+for (i = 0; num > 0; i++)
+{
+num = num / base;
+}
+return (i);
 }
 
 /**
@@ -73,9 +73,9 @@ unsigned int base_len(unsigned int num, int base)
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-  unsigned int i;
-  for (i = 0; i < n; i++)
-    dest[i] = src[i];
-  dest[i] = '\0';
-  return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
 }
