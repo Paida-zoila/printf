3a37512601bb0e1607787e181b3be50dd10ccd3e
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-27 17:49:07 -0700

    returns int to signed decimal

--- helper_functions.c
+++ helper_functions.c
@@ -1,81 +1,37 @@
 #include "holberton.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
-* rev_string - reverses a string in place
-*
-* @s: string to reverse
-* Return: A pointer to a character
-*/
-
-char *rev_string(char *s)
-
+ * format_d - function that returns an int to signed decimal
+ * @valist: arguments passed
+ * @buffer: values stored
+ * @index: tracks index position
+ */
+void format_d(va_list valist, char *buffer, int *index)
 {
-  int len;
- int head; 
-  char tmp;  
-  char *dest;
+	int i, j, numlen;
+	char *num_str;
 
-  for (len = 0; s[len] != '\0'; len++)
-    {}
-  dest = malloc(sizeof(char) * len + 1);
-  if (dest == NULL)
-    return (NULL);
-  _memcpy(dest, s, len);
-  for (head = 0; head < len; head++, len--)
-    {
-      tmp = dest[len - 1];
-      dest[len - 1] = dest[head]; 
-      dest[head] = tmp;
-    }
-  return (dest);
-}
+	i = va_arg(valist, int);
 
-/**
-* write_base - sends characters to be written on standard output
-* @str: String to parse
-*/
-
-void write_base(char *str)
-
-{
-  int i;
-  for (i = 0; str[i] != '\0'; i++)
-    _putchar(str[i]);
-}
-
-/**
-* base_len - Calculates the length for an octal number
-* @num: The number for which the length is being calculated
-* @base: Base to be calculated by
-* Return: An integer representing the length of a number
-*/
-
-unsigned int base_len(unsigned int num, int base)
-
-{
-  unsigned int i;
-  for (i = 0; num > 0; i++)
-    {
-      num = num / base; 
-    }
-  return (i);
-}
-
-/**
-* _memcpy - copy memory area
-* @dest: Destination for copying
-* @src: Source to copy from
-* @n: The number of bytes to copy
-* Return: The _memcpy() function returns a pointer to dest.
-*/
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-
-{
-  unsigned int i;
-  for (i = 0; i < n; i++)
-    dest[i] = src[i];
-  dest[i] = '\0';
-  return (dest);
+	numlen = num_len(i);
+	if (i < 0)
+	{
+		numlen += 1;
+	}
+	num_str = malloc(numlen * sizeof(char));
+	if (num_str == NULL)
+		return;
+	itos(num_str, i);
+	for (i = *index, j = 0; j < numlen; *index += 1, i++, j++)
+	{
+		if (*index == 1024)
+		{
+			_write_buffer(buffer, index);
+			reset_buffer(buffer);
+			*index = 0;
+		}
+		buffer[*index] = num_str[j];
+	}
+	free(num_str);
 }
