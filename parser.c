73d07f8ed4e28b33f3892ac832a2b641da2c8f46
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-27 17:52:49 -0700

    lengths and conversion of strings to integer

--- parser.c
+++ parser.c
@@ -1,54 +1,116 @@
-#include "holberton.h"
+#include <stdio.h>
+/**
+ * itos - integer to string
+ * @str: char array
+ * @n: number
+ *
+ * Return: char array
+ */
+char *itos(char str[], long int n)
+{
+	int i, temp, div, length, last;
+
+	last = n % 10;
+	i = 0;
+	if (last < 0)
+	{
+		last *= -1;
+		str[i] = '-';
+		i++;
+	}
+	n = (n / 10);
+	if (n < 0)
+	{
+		n *= -1;
+	}
+
+	length = 0;
+	div = 1;
+	temp = n;
+
+	while (temp > 0)
+	{
+		length += 1;
+		div *= 10;
+		temp = temp / 10;
+	}
+
+	div = div / 10;
+	while (div >= 1)
+	{
+		str[i] = ((n / div) + '0');
+		n = n % div;
+		div /= 10;
+		i++;
+	}
+	str[i] = last + '0';
+	return (str);
+}
 
 /**
-* parser - Receives the main string and all the necessary parameters to
-* print a formated string.
-* @format: A string containing all the desired characters.
-* @f_list: A list of all the posible functions.
-* @arg_list: A list containing all the argumentents passed to the program.
- * Return: A total count of the characters printed.
-*/
-
-int parser(const char *format, conver_t f_list[], va_list arg_list)
-
+ * utos - uint to string
+ * @str: string
+ * @num: num
+ *
+ * Return: char array
+ */
+char *utos(char str[], unsigned int num)
 {
-  int i, j, r_val, printed_chars;
+	int i, rem, len = 0, n;
 
-  printed_chars = 0;
-  for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
-    {
-      if (format[i] == '%') /*Checks for format specifiers*/
+	n = num;
+	while (n != 0)
 	{
-	  /*Iterates through struct to find the right func*/
-	  for (j = 0; f_list[j].y != NULL; j++)
-	    {
-	      if (format[i + 1] == f_list[j].y[0])
-		{
-		  r_val = f_list[j].f(arg_list);
-		  if (r_val == -1)
-		    return (-1);
-		  printed_chars += r_val;
-		  break; 
-		} 
-	    }
-	  if (f_list[j].y == NULL && format[i + 1] != ' ')
-	    {
-	      if (format[i + 1] != '\0')
-		{
-		  _putchar(format[i]);
-		  _putchar(format[i + 1]);
-		  printed_chars = printed_chars + 2; 
-		}
-	      else
-		return (-1); 
-	    }
-	  i = i + 1; /*Updating i to skip format symbols*/ 
+		len++;
+		n /= 10;
 	}
-      else
+	i = 0;
+
+	while (i < len)
 	{
-	  _putchar(format[i]); /*call the write function*/
-	  printed_chars++; 
-	} 
-    }
-  return (printed_chars);
+		rem = num % 10;
+		num = num / 10;
+
+		str[len - (i + 1)] = rem + '0';
+		i++;
+	}
+
+	str[len] = '\0';
+	return (str);
+}
+/**
+ * num_len - getting the length of a number
+ * @num: the number
+ *
+ * Return: int
+ */
+int num_len(int num)
+{
+	int n, len;
+
+	n = num;
+	len = 0;
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+/**
+ * _strlen - count the length of a string
+ *  @s: char pointer
+ *
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int i, count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (i);
 }
