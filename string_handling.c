8cc604a7300838ea7000707557c43c30b6655f86
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-27 17:43:49 -0700

    strings

--- string_handling.c
+++ string_handling.c
@@ -1,60 +1,116 @@
-#include "holberton.h"
-
+#include <stdio.h>
 /**
-* print_reversed - Calls a function to reverse and print a string
-* @arg: Argument passed to the function
-* Return: The amount of characters printed
-*/
-
-int print_reversed(va_list arg)
-
+ * itos - integer to string
+ * @str: char array
+ * @n: number
+ *
+ * Return: char array
+ */
+char *itos(char str[], long int n)
 {
-	int len;
-	char *str;
-	char *ptr;
+	int i, temp, div, length, last;
 
-	str = va_arg(arg, char *);
-	if (str == NULL)
-	return (-1);
-	ptr = rev_string(str);
-	if (ptr == NULL)
-	return (-1);
-	for (len = 0; ptr[len] != '\0'; len++)
-	_putchar(ptr[len]);
-	free(ptr);
-	return (len);
+	last = n % 10;
+	i = 0;
+	if (last < 0)
+	{
+		last *= -1;
+		str[i] = '-';
+		i++;
+	}
+	n = (n / 10);
+	if (n < 0)
+	{
+		n *= -1;
+	}
+
+	length = 0;
+	div = 1;
+	temp = n;
+
+	while (temp > 0)
+	{
+		length += 1;
+		div *= 10;
+		temp = temp / 10;
+	}
+
+	div = div / 10;
+	while (div >= 1)
+	{
+		str[i] = ((n / div) + '0');
+		n = n % div;
+		div /= 10;
+		i++;
+	}
+	str[i] = last + '0';
+	return (str);
 }
 
 /**
-* rot13 - Converts string to rot13
-* @list: string to convert
-* Return: converted string
-*/
-
-int rot13(va_list list)
-
+ * utos - uint to string
+ * @str: string
+ * @num: num
+ *
+ * Return: char array
+ */
+char *utos(char str[], unsigned int num)
 {
-	int i;
-	int x;
-	char *str;
-	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, rem, len = 0, n;
 
-	str = va_arg(list, char *);
-	if (str == NULL)
-	return (-1);
-	for (i = 0; str[i] != '\0'; i++)
+	n = num;
+	while (n != 0)
 	{
-	for (x = 0; x <= 52; x++)
-	{
-	if (str[i] == s[x])
-		{
-		_putchar(u[x]);
-		break;
-		}
+		len++;
+		n /= 10;
 	}
-	if (x == 53)
-	_putchar(str[i]);
+	i = 0;
+
+	while (i < len)
+	{
+		rem = num % 10;
+		num = num / 10;
+
+		str[len - (i + 1)] = rem + '0';
+		i++;
+	}
+
+	str[len] = '\0';
+	return (str);
+}
+/**
+ * num_len - getting the length of a number
+ * @num: the number
+ *
+ * Return: int
+ */
+int num_len(int num)
+{
+	int n, len;
+
+	n = num;
+	len = 0;
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+/**
+ * _strlen - count the length of a string
+ *  @s: char pointer
+ *
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int i, count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
 	}
 	return (i);
 }
