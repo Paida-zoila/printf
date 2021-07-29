aab0413daaa23dd56393a0cda2d7f162ec2ea666
Imaa Muheto <hopeimaa97@outlook.com>
2021-07-29 05:35:59 -0700

    contains protypes

--- holberton.h
+++ holberton.h
@@ -1,46 +1,32 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
-#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
 /**
- *struct convert - defines a structure for symbols and functions
- *
- * @y :  operator
- * @f: function
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-
-struct convert
+typedef struct print
 {
-	char *y;
+	char *t;
 	int (*f)(va_list);
+} print_t;
 
-};
-typedef struct convert conver_t;
-
-/* functions for the project*/
-int _printf(const char *format, ...);
 int _putchar(char c);
-int parser(const char *format, conver_t f_list[], va_list arg_list);
-int print_char(va_list); /*c*/
-int print_string(va_list); /*s*/
-int print_percent(va_list);/*%*/
-int print_decimal_integer(va_list); /*d*/
-int print_integer(va_list); /*i*/
-int print_octal(va_list list);/*o*/
-int print_hex(va_list list);/*x*/
-int print_HEX(va_list list);/*X*/
-int print_binary(va_list);/*b*/
-int print_unsigned_integer(va_list);/*u*/
-int print_reversed_string(va_list arg);/*r*/
-int print_pointer(va_list);/*p*/
-int rot13(va_list);/*R*/
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-/* helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
-#endif/*holberton.h*/
+#endif  /* _HOLBERTON_H */
